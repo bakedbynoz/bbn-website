34fc7c8095f27529730a00fc0098a670df674ccb
bakedbynoz <bakedbynoz@gmail.com>
2026-07-23 12:36:47 +1000

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Baked by Noz";
-export const SITE_DESCRIPTION = "Home of delicious custom fondant sugar cookies";
+export const SITE_TITLE = 'Baked By Noz';
+export const SITE_DESCRIPTION = 'Handcrafted custom cookies for every celebration.';
